8e9243cd439da4a87d06c0435ff0c3acafb099b5
anthonysiggia <143551868+anthonysiggia@users.noreply.github.com>
2023-11-17 19:58:53 -0500

    Add files via upload

---
+++ bulllet.java
@@ -0,0 +1,68 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class bulllet extends Actor
+{
+    private Point2D position;
+    private Vector2D velocity;
+    private Vector2D acceleration;
+    private static final double GRAVITY = 7 * 200;
+
+    /**
+     * 
+     */
+    public bulllet()
+    {
+        position = null;
+        velocity =  new Vector2D(0.0, 0.0);
+        acceleration =  new Vector2D(0.0, GRAVITY);
+    }
+
+    /**
+     * 
+     */
+    public void act()
+    {
+        updatePhysics();
+        Actor alien = getOneIntersectingObject(alien.class);
+        if (alien != null) {
+            World forrest = getWorld();
+            forrest.removeObject(alien);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void setVelocity(Vector2D newValue)
+    {
+        velocity = newValue;
+    }
+
+    /**
+     * 
+     */
+    public void updatePhysics()
+    {
+        /* Initial position*/
+        if (position == null) {
+            position =  new Point2D(getX(), getY());
+        }
+        /* Get time step duration*/
+        forrest world = (forrest)getWorld();
+        double dt = world.getTimeStepDuration();
+        /* Update velocity*/
+        Vector2D velocityVariation = Vector2D.multiply(acceleration, dt);
+        velocity = Vector2D.add(velocity, velocityVariation);
+        /* Update position*/
+        Vector2D positionVariation = Vector2D.multiply(velocity, dt);
+        position.add(positionVariation);
+        /* Set new actor position*/
+        setLocation((int)position.getX(), (int)position.getY());
+    }
+}
---
+++ gameOverWonWorld.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class gameOverWonWorld extends World
+{
+
+    /**
+     * Constructor for objects of class gameOverWonWorld.
+     */
+    public gameOverWonWorld()
+    {
+        super(355, 400, 1);
+    }
+}
