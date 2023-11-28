dfd98303df03975ba23aa5ef09982c15d313ae03
anthonysiggia1 <143552080+anthonysiggia1@users.noreply.github.com>
2023-11-28 14:03:01 -0500

    alien 1

--- bulllet.java
+++ bulllet.java
@@ -19,8 +19,8 @@ public class bulllet extends Actor
     public bulllet()
     {
         position = null;
-        velocity =  new Vector2D(0.0, 0.0);
-        acceleration =  new Vector2D(0.0, GRAVITY);
+        velocity =  new  Vector2D(0.0, 0.0);
+        acceleration =  new  Vector2D(0.0, GRAVITY);
     }
 
     /**
@@ -29,10 +29,10 @@ public class bulllet extends Actor
     public void act()
     {
         updatePhysics();
-        Actor alien = getOneIntersectingObject(alien.class);
-        if (alien != null) {
+        Actor alien1 = getOneIntersectingObject(alien1.class);
+        if (alien1 != null) {
             World forrest = getWorld();
-            forrest.removeObject(alien);
+            forrest.removeObject(alien1);
         }
     }
 
@@ -51,7 +51,7 @@ public class bulllet extends Actor
     {
         /* Initial position*/
         if (position == null) {
-            position =  new Point2D(getX(), getY());
+            position =  new  Point2D(getX(), getY());
         }
         /* Get time step duration*/
         forrest world = (forrest)getWorld();
